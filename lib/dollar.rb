a0cba67171ab4f5bff2fb1d3e22c17c1615fb421
Pietro Di Bello <orione-team+dip@xpeppers.com>
2012-02-24 14:29:09 +0100

    moved spec in the proper dollar spec file

---
+++ lib/dollar.rb
@@ -0,0 +1,11 @@
+class Dollar
+  def initialize(amount)  
+  end
+  
+  def amount
+    10
+  end
+  
+  def times(how_many)  
+  end
+end
--- tdd_money_example.rb
+++ tdd_money_example.rb
@@ -1,18 +1,3 @@
 require "rubygems"
 require "bundler"
 Bundler.setup
-
-class TddMoneyExample
-end
-
-class Dollar
-  def initialize(amount)  
-  end
-  
-  def amount
-    10
-  end
-  
-  def times(how_many)  
-  end
-end
